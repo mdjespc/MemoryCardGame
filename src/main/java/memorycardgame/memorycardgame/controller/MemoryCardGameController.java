116ea74daa8fe35be2886858a65a9ea902c36966
mdjespc <mdjespc@gmail.com>
2024-09-12 01:13:34 -0400

    New implementation of controller initializer.

--- src/main/java/memorycardgame/memorycardgame/controller/MemoryCardGameController.java
+++ src/main/java/memorycardgame/memorycardgame/controller/MemoryCardGameController.java
@@ -1,10 +1,8 @@
 package memorycardgame.memorycardgame.controller;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -18,10 +16,11 @@ import memorycardgame.memorycardgame.model.Card;
 import memorycardgame.memorycardgame.model.MemoryGameCard;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MemoryCardGameController implements Initializable {
 
@@ -37,7 +36,7 @@ public class MemoryCardGameController implements Initializable {
     @FXML
     private Button resetButton;
 
-    private ArrayList<MemoryGameCard> memoryGameCards;
+    private List<MemoryGameCard> memoryGameCards;
 
     @FXML
     void reset(ActionEvent event) {
@@ -46,26 +45,30 @@ public class MemoryCardGameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        memoryGameCards = new ArrayList<>();
         Deck deck = new Deck();
         deck.shuffleDeck();
 
-        for (int i = 0; i < imageFlowPane.getChildren().size() / 2; i++){
-            //Add two copies of the dealt card to list
-            Card cardDealt = deck.dealCard();
-            MemoryGameCard  memoryGameCardDealt = new MemoryGameCard(cardDealt.getRank(), cardDealt.getSuit());
-            memoryGameCards.add(memoryGameCardDealt);
-            memoryGameCards.add(memoryGameCardDealt);
-        }
+        //Populate the memoryGameCards list by adding two copies of each dealt card
+        memoryGameCards = IntStream.range(0, imageFlowPane.getChildren().size() / 2)
+                .mapToObj(i -> {
+                    Card dealtCard = deck.dealCard();
+                    MemoryGameCard memoryGameCard = new MemoryGameCard(dealtCard.getRank(), dealtCard.getSuit());
+                    return List.of(memoryGameCard, memoryGameCard);
+                })
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        //Shuffle the memoryGameCards list
         Collections.shuffle(memoryGameCards);
 
-        for (int i = 0; i < imageFlowPane.getChildren().size(); i++){
-            MemoryGameCard displayedCard = memoryGameCards.get(i);
-            String imagePath = String.format("images/%s_%s.png", displayedCard.getRank(), displayedCard.getSuit());
-            ImageView imageView = (ImageView) imageFlowPane.getChildren().get(i);
-            imageView.setImage(new Image(App.class.getResourceAsStream(imagePath)));
-        }
-
+        //Assign images to each imageView in imageFlowPane
+        IntStream.range(0, imageFlowPane.getChildren().size())
+                .forEach(i -> {
+                    MemoryGameCard displayedCard = memoryGameCards.get(i);
+                    String imagePath = String.format("images/%s_%s.png", displayedCard.getRank(), displayedCard.getSuit());
+                    ImageView imageView = (ImageView) imageFlowPane.getChildren().get(i);
+                    imageView.setImage(new Image(App.class.getResourceAsStream(imagePath)));
+                });
     }
 }
 
