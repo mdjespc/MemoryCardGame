d9cbb7061be7e8fa577adefd85749eea4f630a5e
mdjespc <mdjespc@gmail.com>
2024-09-13 01:20:43 -0400

    Implemented full controller logic.

--- src/main/java/memorycardgame/memorycardgame/controller/MemoryCardGameController.java
+++ src/main/java/memorycardgame/memorycardgame/controller/MemoryCardGameController.java
@@ -38,13 +38,22 @@ public class MemoryCardGameController implements Initializable {
 
     private List<MemoryGameCard> memoryGameCards;
 
+    private MemoryGameCard firstFlippedCard;
+    private MemoryGameCard secondFlippedCard;
+
+    private int guessesLabelValue;
+    private int matchesLabelValue;
+
+
+
     @FXML
     void reset(ActionEvent event) {
+        firstFlippedCard = null;
+        secondFlippedCard = null;
 
-    }
+        guessesLabelValue = 0;
+        matchesLabelValue = 0;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
         Deck deck = new Deck();
         deck.shuffleDeck();
 
@@ -61,14 +70,90 @@ public class MemoryCardGameController implements Initializable {
         //Shuffle the memoryGameCards list
         Collections.shuffle(memoryGameCards);
 
-        //Assign images to each imageView in imageFlowPane
+        //Assign images and event listeners to each imageView in imageFlowPane
         IntStream.range(0, imageFlowPane.getChildren().size())
                 .forEach(i -> {
                     MemoryGameCard displayedCard = memoryGameCards.get(i);
-                    String imagePath = String.format("images/%s_%s.png", displayedCard.getRank(), displayedCard.getSuit());
                     ImageView imageView = (ImageView) imageFlowPane.getChildren().get(i);
-                    imageView.setImage(new Image(App.class.getResourceAsStream(imagePath)));
+                    imageView.setImage(getCardBackImage());
+
+                    imageView.setUserData(i);
+
+                    //Set click event listener
+                    imageView.setOnMouseClicked(mouseEvent -> onMemoryGameCardClicked(displayedCard, imageView));
                 });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        reset(new ActionEvent());
+    }
+
+    Image getCardImage(Card card){
+        return new Image(App.class.getResourceAsStream(String.format("images/%s_%s.png", card.getRank(), card.getSuit())));
+    }
+
+    Image getCardBackImage(){
+        return new Image(App.class.getResourceAsStream("images/card_back_red.png"));
+    }
+
+    private void updateGuessesLabel(int value){
+        guessesLabel.setText(Integer.toString(value));
+    }
+
+    private void updateMatchesLabel(int value){
+        matchesLabel.setText(Integer.toString(value));
+    }
+
+    /**
+     * Event handler that handles the logic of a mouse press on a non-matched dealt card.
+     * @param clickedCard
+     * @param clickedImageView
+     */
+    private void onMemoryGameCardClicked(MemoryGameCard clickedCard, ImageView clickedImageView){
+        if (clickedCard.isMatched())
+            return;
+
+        if (firstFlippedCard == null)
+            firstFlippedCard = clickedCard;
+        else if (secondFlippedCard == null) {
+            secondFlippedCard = clickedCard;
+            findMatch();
+        }
+        else {
+            showAllCardBacks();
+            firstFlippedCard = clickedCard;
+            secondFlippedCard = null;
+        }
+
+        clickedImageView.setImage(getCardImage(clickedCard));
+    }
+
+    /**
+     * Checks for a match between first and second card selected.
+     */
+    private void findMatch(){
+        if (firstFlippedCard.matches(secondFlippedCard)){
+            updateMatchesLabel(++matchesLabelValue);
+            firstFlippedCard.setMatched(true);
+            secondFlippedCard.setMatched(true);
+        }
+        else{
+            updateGuessesLabel(++guessesLabelValue);
+            firstFlippedCard.setMatched(false);
+            secondFlippedCard.setMatched(false);
+        }
+    }
+
+    /**
+     * Sets all unmatched cards back to their un-flipped image states (card backs).
+     */
+    private void showAllCardBacks(){
+        imageFlowPane.getChildren().forEach(node ->{
+            ImageView imageView = (ImageView) node;
+            if (!memoryGameCards.get((int) imageView.getUserData()).isMatched())
+                imageView.setImage(getCardBackImage());
+        });
     }
 }
 
